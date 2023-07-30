19405df60d1e984b0115b60e3d09f26cb2ec9e06
GalKepler <galkepler@gmail.com>
2023-07-30 10:51:40 +0300

    added basic version and metadata

---
+++ keprep/__about__.py
@@ -0,0 +1,20 @@
+"""Base module variables."""
+
+try:
+    from keprep._version import __version__
+except ImportError:
+    __version__ = "0+unknown"
+
+__packagename__ = "keprep"
+__copyright__ = "Copyright 2023, Gal Kepler"
+__credits__ = (
+    "Contributors: please check the ``.zenodo.json`` file at the top-level folder"
+    "of the repository"
+)
+__url__ = "https://github.com/GalKepler/keprep"
+
+DOWNLOAD_URL = (
+    "https://github.com/GalKepler/keprep/{name}/archive/{ver}.tar.gz".format(
+        name=__packagename__, ver=__version__
+    )
+)
--- keprep/__init__.py
+++ keprep/__init__.py
@@ -1,9 +1,31 @@
-from importlib.metadata import PackageNotFoundError, version
+# type: ignore[attr-defined]
+"""Post-processing for QSIPrep's outputs"""
 
-try:
-    __version__ = version("KePrep")
-except PackageNotFoundError:
-    __version__ = "(local)"
+from importlib import metadata as importlib_metadata
 
-del PackageNotFoundError
-del version
+from keprep.__about__ import (
+    __copyright__,
+    __credits__,
+    __packagename__,
+    __version__,
+)
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
+
+"""Top-module metadata."""
+
+
+__all__ = [
+    "__copyright__",
+    "__credits__",
+    "__packagename__",
+    "__version__",
+]
